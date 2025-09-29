473d4132329382dc677969bd2d4271c5dada6cff
xxubin04 <yubinjeong40@gmail.com>
2025-09-29 18:03:02 +0900

    chore: 초기 세팅

---
+++ todak-server/src/main/java/com/example/todak_server/TodakServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.todak_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodakServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodakServerApplication.class, args);
+	}
+
+}
---
+++ todak-server/src/test/java/com/example/todak_server/TodakServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todak_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodakServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
