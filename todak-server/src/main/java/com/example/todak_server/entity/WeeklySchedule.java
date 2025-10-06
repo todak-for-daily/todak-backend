f61cc186625b827b7a47c1dead528395904ba45b
Dolchae <chch4607@naver.com>
2025-10-06 19:10:45 +0900

    feat: weekly(일주일 시간표) 엔티티 및 enum 생성

---
+++ todak-server/src/main/java/com/example/todak_server/entity/WeeklySchedule.java
@@ -0,0 +1,81 @@
+package com.example.todak_server.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalTime;
+
+@Entity
+public class WeeklySchedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Member member;
+
+    @Enumerated
+    private DayOfWeek dayOfWeek; //월~일
+
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    private String title;
+    private String color;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(DayOfWeek dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
