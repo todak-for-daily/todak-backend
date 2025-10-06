1ab559778ac1714239b7ad321e9b6003157242f1
Dolchae <chch4607@naver.com>
2025-10-06 19:13:39 +0900

    feat: 레포지토리 생성

---
+++ todak-server/src/main/java/com/example/todak_server/repository/GeneralScheduleRepository.java
@@ -0,0 +1,20 @@
+package com.example.todak_server.repository;
+
+import com.example.todak_server.entity.GeneralSchedule;
+import com.example.todak_server.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface GeneralScheduleRepository extends JpaRepository<GeneralSchedule,Long> {
+
+    // 특정 사용자의 일정 전체 조회
+    List<GeneralSchedule> findByMember(Member member);
+
+    // 특정 사용자의 일정 중 특정 기간 조회 (ex. 먼슬리 캘린더 ..)
+    List<GeneralSchedule> findByMemberAndDateBetween(Member member, LocalDate start, LocalDate end);
+
+    // sourceId로 연결된 general 일정 찾기 (일주일 시간표로 생성된 일정들 찾기.)
+    List<GeneralSchedule> findBySourceId(Long sourceId);
+}
