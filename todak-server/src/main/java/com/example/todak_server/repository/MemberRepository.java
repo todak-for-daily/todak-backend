f0232f38ab8c97383f251849a8de9cd856bee509
xxubin04 <yubinjeong40@gmail.com>
2025-10-15 01:50:28 +0900

    refactor: findByEmail 함수 추가

--- todak-server/src/main/java/com/example/todak_server/repository/MemberRepository.java
+++ todak-server/src/main/java/com/example/todak_server/repository/MemberRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,String> {
     Optional<Member> findByProviderAndProviderId(String provider, String providerId);
+    Optional<Member> findByEmail(String email);
 }
