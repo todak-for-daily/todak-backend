6d4694dcc66a4ab44d40b88ef13bd53d5fc27509
Dolchae <chch4607@naver.com>
2025-11-16 21:09:18 +0900

    docs: 사용자 이미지 관련 문서화

--- todak-server/src/main/java/com/example/todak_server/controller/AvatarController.java
+++ todak-server/src/main/java/com/example/todak_server/controller/AvatarController.java
@@ -2,6 +2,8 @@ package com.example.todak_server.controller;
 
 import com.example.todak_server.file.FirebaseStorageUploader;
 import com.example.todak_server.service.AvatarService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
+@Tag(name = "사용자 본인 이미지", description = "사용자 본인의 이미지 등록 및 가져오기")
 @RestController
 @RequiredArgsConstructor
 public class AvatarController {
@@ -21,6 +24,7 @@ public class AvatarController {
     private final FirebaseStorageUploader uploader;
     private final AvatarService memberService;
 
+    @Operation(summary = "사용자 이미지 등록")
     @PostMapping(value = "/api/me/avatar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<Map<String, String>> uploadAvatar(
             @AuthenticationPrincipal(expression = "id") Long memberId,
@@ -31,6 +35,7 @@ public class AvatarController {
         return ResponseEntity.ok().body(Map.of("imageUrl", imageUrl));
     }
 
+    @Operation(summary = "사용자 이미지 불러오기")
     @GetMapping("/api/me/avatar")
     public ResponseEntity<Map<String, String>> getAvatar(
             @AuthenticationPrincipal(expression = "id") Long memberId
